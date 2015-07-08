1b12397b2c4c07c1de589554d30cdc1e6cc93c1a
quannt <simpleplan.vn@gmail.com>
2015-07-09 00:05:52 +0800

    first commit

---
+++ Backbone/js/models/book.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.Book = Backbone.Model.extend({
+	defaults:{
+		coverImage:'img/placeholder.png',
+		title:'No tile',
+		author:'Unknown',
+		releaseDate:'Unknown',
+		keywords:'None'
+	}
+});
